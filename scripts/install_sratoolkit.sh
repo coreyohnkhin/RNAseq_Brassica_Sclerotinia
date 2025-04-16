6ff88b04d9e1e2b73dfe87514cbefca4dcce8e6e
Corey <coreyok@pm.me>
2025-04-16 01:35:06 +0000

    Update SRA Toolkit install.

--- scripts/install_sratoolkit.sh
+++ scripts/install_sratoolkit.sh
@@ -3,3 +3,4 @@ cd software
 wget --output-document sratoolkit.tar.gz https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/current/sratoolkit.current-ubuntu64.tar.gz
 tar -vxzf sratoolkit.tar.gz
 rm sratoolkit.tar.gz
+cd ..
